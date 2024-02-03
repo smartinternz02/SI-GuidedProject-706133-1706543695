8f7ccc7cf18c0b11927ae572490d3a6e58300e2e
Varshith-Kali <pulivarshit@gmail.com>
2024-02-03 22:00:56 +0530

    Implement test case on custom keywords and generate reports

---
+++ Project Files/Keywords/custom_keywords_pack/customFunction.groovy
@@ -0,0 +1,39 @@
+package custom_keywords_pack
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+
+public class customFunction {
+	
+	@Keyword
+	def token() {
+		println("\n<< You have generated a token >>\n")
+	}
+	
+	@Keyword
+	def issue() {
+		println("\n<< Your issue has been resolved >>\n")
+	}
+	
+	@Keyword
+	def userName(String name) {
+		println("\n..... Hey " + name + " ! .....\n")
+	}
+}
